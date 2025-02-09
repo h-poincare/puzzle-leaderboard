051a239d070b6f34820e34c28edd233b23686adc
adam_shetler <adam.d.shetler@gmail.com>
2025-02-09 13:26:41 -0500

    testing

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '/opt/render/app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
